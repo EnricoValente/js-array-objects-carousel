c6b7d1417ab9dc482b90eb1b645d4d3ca42be9f1
EnricoValente <enricovalente42@gmail.com>
2023-06-27 22:06:14 +0200

    creato if e stampata immagine in pag

--- js/script.js
+++ js/script.js
@@ -25,19 +25,23 @@ const carouselContent = [
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
+    }, 
+    {
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
+    }, 
+    {
         image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
+    },
+     {
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
+    }, 
+    {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
@@ -48,28 +52,71 @@ const carouselContent = [
 const imagesContainer = document.querySelector('.images-container');
 
 carouselContent.forEach((object, i) => {
-    //console.log('test',object, i);
+    console.log('test',object, i);
 
 
-  
+    if (i == 0) {
+        
+        imagesContainer.innerHTML+= `
+        <div class="image-item active"> 
+
+          <img src="${object['image']}" alt="">
+
+          
+            <div class="images-text">
+        
+                <h3>    
+                    ${object['title']}
+                </h3>
+
+                <p>
+                 ${object['text']}  
+                </p>
+
+            </div>    
+          
+        </div> 
+
+       
+        `
+    } 
+              
+    
+              
+    else {
+
+        imagesContainer.innerHTML+= `
+        <div class="image-item"> 
+
+          <img src="${object['image']}" alt="">
+
+
+             <div class="images-text">
+
+            
+                <h3>    
+                  ${object['title']}
+                </h3>
+
+                <p>
+                 ${object['text']}  
+                </p>
+
+            </div>    
+
+        </div>
+
+        `
+    }
 
     for (const key in object) {
-        console.log(object[key]);
+        // console.log(object[key]);
     }
 
 });
 
 
 
-
-
-
-
-
-
-            
-          
-          
 
 
 
